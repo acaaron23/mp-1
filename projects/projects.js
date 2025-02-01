1278ef99ea6105a2fa6d273283e4ff58946acc2a
Aaron Chen <acaaron@bu.edu>
2025-02-01 17:24:27 -0500

    Working on JS Calculator function

---
+++ projects/projects.js
@@ -0,0 +1,41 @@
+function addition() {
+    const numberInput1 = document.getElementById("first-number").value
+    const numberInput2 = document.getElementById("second-number").value
+
+    const number1 = Number(numberInput1);
+    const number2 = Number(numberInput2);
+    document.getElementById("result-msg").innerHTML = String(number1 + number2);
+}
+
+function subtract() {
+    const numberInput1 = document.getElementById("first-number").value
+    const numberInput2 = document.getElementById("second-number").value
+
+    const number1 = Number(numberInput1);
+    const number2 = Number(numberInput2);
+    document.getElementById("result-msg").innerHTML = String(number1 - number2);
+}
+
+function multiply() {
+    const numberInput1 = document.getElementById("first-number").value
+    const numberInput2 = document.getElementById("second-number").value
+
+    const number1 = Number(numberInput1);
+    const number2 = Number(numberInput2);
+    document.getElementById("result-msg").innerHTML = String(number1 * number2);
+}
+
+function divide() {
+    const numberInput1 = document.getElementById("first-number").value
+    const numberInput2 = document.getElementById("second-number").value
+
+    const number1 = Number(numberInput1);
+    const number2 = Number(numberInput2);
+
+    // Edge case: When number 2 is equal to 0
+    if(number2 === 0){
+        document.getElementById("result-msg").innerHTML = "Cannot divide by 0";
+    }
+    document.getElementById("result-msg").innerHTML = String(number1 / number2);
+}
+
